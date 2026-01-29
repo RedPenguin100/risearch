dbf7c30331d6ef3dd186e8fe6df896f76b00fed3
Michael Kovaliov <32510658+RedPenguin100@users.noreply.github.com>
2026-01-29 16:52:02 +0200

    Remove commented transpose logic
    Removed commented-out transpose logic from the code.

--- RIsearch1/risearch.c
+++ RIsearch1/risearch.c
@@ -743,7 +743,6 @@ getMat (char *matname, short *bA_nu)
 
 	if (transpose_matrix)
 	{
-		/* Option 1: Transpose logic [k][l][i][j] = [i][j][k][l] */
 		for (i = 0; i < 6; i++) {
 			for (j = 0; j < 6; j++) {
 				for (k = 0; k < 6; k++) {
